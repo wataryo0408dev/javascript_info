02177914798cadfd671ea1e5d966d184fc3feeb5
ryo watanabe <ryo.watanabe@theport.jp>
2024-02-07 18:02:44 +0900

    add main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+// let admin;
+// let name;
+// name = 'Jone';
+// admin = name;
+// alert(name);
+
+// let ourPlanetName = 'earth'
+// let studentName = 'ryo watanabe'
+
+let name = "Ilya"
+
+alert(`hello ${1}`);
+alert(`hello ${"name"}`);
+alert(`hello ${name}`);
+
